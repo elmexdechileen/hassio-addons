36b08a04fbcbb034fd4323a8c9a9435d67af846f
elmexdechileen <maxvanrooijen@gmail.com>
2017-12-21 22:01:19 +0100

    Fixed runsh

--- telnetGPIO/run.sh
+++ telnetGPIO/run.sh
@@ -1,1 +1,1 @@
-python3 /usr/opt/telnetmqtt/startDaemon.py  "192.168.1.148" 8883 "telnetA" "192.168.1.171" .1
+python3 /usr/opt/telnetmqtt/telnetGPIODaemon.py  "192.168.1.148" 8883 "telnetA" "192.168.1.171" .1
